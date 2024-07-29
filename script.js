93993323076735ab87d734b3f80ca1404195866f
Weejan <weezan49@gmail.com>
2024-07-30 00:21:50 +0545

    Add human choice function

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+let para = document.querySelector("p");
+
+    //btnChoice[i].addEventListener("click", getComputerChoice());
+
+
 function getComputerChoice(){
     let choices = ['rock','paper','scissors']
     let choice = choices[Math.floor(Math.random * choices.length)]
     return choice
 }
 
+function getHumanChoice(){
+    let btnChoice = document.querySelectorAll(".btn");
+    let choice;
+    btnChoice.forEach((item)=> {item.addEventListener("click",()=>{
+        choice = item.value;
+        para.textContent = `You choose ${choice}!`;
+    })})
+    document.body.appendChild(para);
+        
+    }
+
+getHumanChoice()``
